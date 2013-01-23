4fb46416c4690e401d17acc24c7afcd4d02b021b
Dnigor <gorobets.dmitriy@gmail.com>
2013-01-24 00:47:49 +0200

    commit2

---
+++ calc2.Tests/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace calc2.Tests
+{
+
+    [TestClass]
+    public class CalcTests
+    {
+
+        [TestMethod]
+        public void AddOne()
+        {            
+            Assert.AreEqual(1, Calc2.Add(@"1,"));
+        }
+
+        [TestMethod]
+        public void AddTwo()
+        {
+            Assert.AreEqual(3, Calc2.Add(@"1\n2"));
+        }
+
+        [TestMethod]
+        public void AddEmpty()
+        {
+            Assert.AreEqual(0, Calc2.Add(@""));
+        }
+ 
+    }
+}
---
+++ calc2/calc2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calc2
+{
+    public class Calc2
+    {
+       public static int Add(string numbers)
+       {
+           string[] separators = {",", @"\n"};
+           return numbers.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(s =>
+                                                       {
+                                                           int i;
+                                                           Int32.TryParse(s, out i);
+                                                           return i;
+                                                       }).ToArray().Sum();
+       }
+    }
+}
